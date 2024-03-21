5f6d73df4a40446f9d36f4ec975323f8650c9eed
pedohorse <13556996+pedohorse@users.noreply.github.com>
2024-03-21 10:26:23 +0100

    add tests

--- build.zig
+++ build.zig
@@ -59,17 +59,26 @@ pub fn build(b: *std.Build) void {
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
-    const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_unit_tests = b.addRunArtifact(unit_tests);
 
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
+
+    const tests = [_][]const u8{
+        "src/main.zig",
+        "src/random.zig",
+        "src/parallel.zig",
+    };
+    for (tests) |test_file| {
+        const unit_tests = b.addTest(.{
+            .root_source_file = .{ .path = test_file },
+            .target = target,
+            .optimize = optimize,
+        });
+
+        const run_unit_tests = b.addRunArtifact(unit_tests);
+
+        test_step.dependOn(&run_unit_tests.step);
+    }
 }
