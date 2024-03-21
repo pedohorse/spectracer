34f923ff7ad493423e186178925d3467606de138
pedohorse <13556996+pedohorse@users.noreply.github.com>
2024-03-21 10:26:07 +0100

    extract random

--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,7 @@ const embree = @cImport({
 });
 const Pool = @import("parallel.zig").Pool;
 const Color = @import("data_types.zig").Color;
+const random_hemisphere = @import("random.zig").random_hemisphere;
 
 const output_ascii = @import("output_ascii.zig");
 const output_png = @import("output_png.zig");
@@ -208,39 +209,4 @@ fn trace_ray(rayhit: *embree.RTCRayHit, chunk_data: *const ChunkData, rng: std.r
     val *= 0.99;
 
     return val;
-}
-
-fn random_hemisphere(u: f32, v: f32, normal: @Vector(3, f32)) @Vector(3, f32) {
-    const sample = random_sphere(u, v);
-    return if (@reduce(.Add, sample * normal) < 0)
-        -sample
-    else
-        sample;
-}
-
-fn random_sphere(u: f32, v: f32) @Vector(3, f32) {
-    const fi = u * 2 * std.math.pi;
-    var x = @cos(fi);
-    var y = @sin(fi);
-    var z = -1 + 2 * v;
-    var cz = @sqrt(1 - z * z);
-
-    return .{ x * cz, y * cz, z };
-}
-
-test "random_hemisphere" {
-    var randomizer = std.rand.Xoshiro256.init(132435);
-    var rng = randomizer.random();
-    const ray1 = random_hemisphere(rng.float(f32), rng.float(f32), .{ 0, 1, 0 });
-    const ray2 = random_hemisphere(rng.float(f32), rng.float(f32), .{ 0, -1, 0 });
-    const ray3 = random_hemisphere(rng.float(f32), rng.float(f32), .{ 1, 0, 0 });
-    const ray4 = random_hemisphere(rng.float(f32), rng.float(f32), .{ -1, 0, 0 });
-    const ray5 = random_hemisphere(rng.float(f32), rng.float(f32), .{ 0, 0, 1 });
-    const ray6 = random_hemisphere(rng.float(f32), rng.float(f32), .{ 0, 0, -1 });
-    try std.testing.expect(ray1[1] >= 0);
-    try std.testing.expect(ray2[1] <= 0);
-    try std.testing.expect(ray3[0] >= 0);
-    try std.testing.expect(ray4[0] <= 0);
-    try std.testing.expect(ray5[2] >= 0);
-    try std.testing.expect(ray6[2] <= 0);
 }
---
+++ src/random.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+
+pub fn random_hemisphere(u: f32, v: f32, normal: @Vector(3, f32)) @Vector(3, f32) {
+    const sample = random_sphere(u, v);
+    return if (@reduce(.Add, sample * normal) < 0)
+        -sample
+    else
+        sample;
+}
+
+pub fn random_sphere(u: f32, v: f32) @Vector(3, f32) {
+    const fi = u * 2 * std.math.pi;
+    var x = @cos(fi);
+    var y = @sin(fi);
+    var z = -1 + 2 * v;
+    var cz = @sqrt(1 - z * z);
+
+    return .{ x * cz, y * cz, z };
+}
+
+test "random_hemisphere" {
+    var randomizer = std.rand.Xoshiro256.init(132435);
+    var rng = randomizer.random();
+    const ray1 = random_hemisphere(rng.float(f32), rng.float(f32), .{ 0, 1, 0 });
+    const ray2 = random_hemisphere(rng.float(f32), rng.float(f32), .{ 0, -1, 0 });
+    const ray3 = random_hemisphere(rng.float(f32), rng.float(f32), .{ 1, 0, 0 });
+    const ray4 = random_hemisphere(rng.float(f32), rng.float(f32), .{ -1, 0, 0 });
+    const ray5 = random_hemisphere(rng.float(f32), rng.float(f32), .{ 0, 0, 1 });
+    const ray6 = random_hemisphere(rng.float(f32), rng.float(f32), .{ 0, 0, -1 });
+    try std.testing.expect(ray1[1] >= 0);
+    try std.testing.expect(ray2[1] <= 0);
+    try std.testing.expect(ray3[0] >= 0);
+    try std.testing.expect(ray4[0] <= 0);
+    try std.testing.expect(ray5[2] >= 0);
+    try std.testing.expect(ray6[2] <= 0);
+}
